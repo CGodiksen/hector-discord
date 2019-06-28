07ab624835b7a8e4109be9857588d47e41bc58b5
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-06-28 05:29:12 +0200

    Added functionality for the event system.
    This is done through the new_event method and the check_events method. Furthermore is a background task handler implemented to run the functionality of the event system through an infinite loop in the background.

--- hector.py
+++ hector.py
@@ -5,14 +5,35 @@
 import discord
 import asyncio
 import csv
+import dateutil.parser
 from datetime import date
+import datetime
 
 
 class Hector(discord.Client):
+    def __init__(self, **options):
+        super().__init__(**options)
+
+        # create the background task and run it in the background
+        self.bg_task = self.loop.create_task(self.my_background_task())
+
     async def on_ready(self):
-        print("We have logged in as " + str(self.user))
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def my_background_task(self):
+        await self.wait_until_ready()
+        while not self.is_closed():
+            if datetime.datetime.now().hour == 8 and datetime.datetime.now().minute == 15:
+                await self.check_events()
+            await asyncio.sleep(60)
 
     async def on_message(self, message):
+        # Obtaining some necessary information about the message
+        server = message.guild
+
         # Ignore if the message is from the bot itself.
         if message.author == self.user:
             return
@@ -23,9 +44,6 @@ class Hector(discord.Client):
         if message.content == "!get_out":
             await self.get_out(message)
 
-        if message.content == "!member_count":
-            await self.member_count(message)
-
         if message.content.startswith("!remind_me ", 0, 11):
             await self.remind_me(message)
 
@@ -35,6 +53,17 @@ class Hector(discord.Client):
         if message.content == "!commands":
             await self.commands(message)
 
+        # Ensuring that these commands can only be called in a server text channel.
+        if type(message.channel) is discord.TextChannel:
+            if message.content == "!member_count":
+                await self.member_count(message, server)
+
+            if message.content.startswith("!new_event ", 0, 11):
+                await self.new_event(message)
+
+            if message.content == "!check_events":
+                await self.check_events()
+
     # Shuts down the bot if the command was made by me (and only me).
     async def get_out(self, message):
         if message.author.id == ***REMOVED***:
@@ -42,9 +71,8 @@ class Hector(discord.Client):
 
     # Sends a message containing the amount of members in a server, if the command request was made in a server.
     @staticmethod
-    async def member_count(message):
+    async def member_count(message, server):
         try:
-            server = message.guild
             await message.channel.send(server.name + " has " + str(len(server.members)) + " members")
         except AttributeError:
             await message.channel.send("This is not a server you fool!")
@@ -99,6 +127,44 @@ class Hector(discord.Client):
                                    "!member_count\n"
                                    "!remind_me ***message*** ***time***\n"
                                    "!birthday")
+
+    # Creating a new event that will be added to the list of events for the specific server
+    @staticmethod
+    async def new_event(message):
+        message_list = message.content.split()
+
+        # Splitting the message into the different parts
+        event_message = " ".join(message_list[1:-1])
+        event_date = message_list[-1]
+
+        # Checking if the given date can be parsed into a datetime.datetime object
+        try:
+            dateutil.parser.parse(message_list[-1])
+        except ValueError:
+            await message.channel.send("This is not a supported date format")
+            return
+
+        # Appending the file that contains the currently active events
+        with open("events.csv", "a", newline="") as fileobject:
+            event_writer = csv.writer(fileobject)
+
+            event_writer.writerow([message.channel.id, event_message, event_date])
+
+    # Looking through the list of events and sending the events that are happening today
+    @staticmethod
+    async def check_events():
+        # Trying to open the file for reading, if it doesnt exist it throws an OSError.
+        try:
+            with open("events.csv", "r") as fileobject:
+                event_reader = csv.reader(fileobject)
+
+                # Goes through all the rows and checks if it scheduled for today
+                for row in event_reader:
+                    if dateutil.parser.parse(row[2]).date() == date.today():
+                        await client.get_channel(int(row[0])).send(" ".join(row[1]))
+
+        except OSError:
+            return
 
 
 client = Hector()
